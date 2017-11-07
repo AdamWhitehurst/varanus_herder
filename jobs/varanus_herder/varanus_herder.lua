c72c6b287089e27fe5b62a5b89e93ff59b5fd52c
Adam Whitehurst <Adam.Whitehurst@live.com>
2017-11-06 18:20:01 -0800

    Added Basic Varanus Herder Job
    Inherits from, and functions exactly like, Shepherd Job, no localization
    yet.

--- jobs/varanus_herder/varanus_herder.lua
+++ jobs/varanus_herder/varanus_herder.lua
@@ -1,35 +1,5 @@
-local rng = _radiant.math.get_default_rng()
-local BaseJob = require 'stonehearth.jobs.base_job'
-local ShepherdClass = require 'stonehearth.jobs.shepherd.shepherd'
-radiant.mixin(ShepherdClass, BaseJob)
 local VaranusHerderClass = class()
+local ShepherdClass = require ('stonehearth.jobs.shepherd.shepherd')
 radiant.mixin(VaranusHerderClass, ShepherdClass)
-
---- Public functions, required for all classes
-function VaranusHerderClass:initialize()
-   BaseJob.initialize(self)
-   ShepherdClass.initialize(self)
-   self._sv.last_found_critter_time = nil
-   self._sv.trailed_animals = nil
-   self._sv.num_trailed_animals = 0
-end
-
-function VaranusHerderClass:demote()
-   ShepherdClass.demote(self)
-
-   --Orphan all the animals
-   self:_abandon_following_animals()
-
-   self.__saved_variables:mark_changed()
-end
-
--- Note we could get destroyed without being demoted, so we do need to clean up on destroy
-function VaranusHerderClass:destroy()
-   if self._sv.is_current_class then
-      self:_abandon_following_animals()
-   end
-
-   ShepherdClass.destroy(self)
-end
-
+-- promote_to varanus_herder:jobs:varanus_herder
 return VaranusHerderClass
